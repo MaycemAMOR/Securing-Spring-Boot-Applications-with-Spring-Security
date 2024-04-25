69c286296653a8e11fbe938be3dd49b9afb4b904
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-25 12:48:23 +0200

    - Creating Spring Security Configuration to Disable CSRF.

---
+++ src/main/java/com/mytech/learnspringsecurity/resources/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.mytech.learnspringsecurity.resources.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> {
+                    auth.anyRequest().authenticated();
+                });
+        http.sessionManagement(
+                session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+        //http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+}
